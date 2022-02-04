4516ecbe12354e9f74c82de057c4462a048a5ba8
Esme-gif <62487373+Esme-gif@users.noreply.github.com>
2022-02-03 21:20:59 -0800

    finished Tutorial

---
+++ NavMesh Example Project/Assets/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera cam;
+
+    public NavMeshAgent agent;
+
+    public ThirdPersonCharacter character;
+
+
+    void Start()
+    {
+        agent.updateRotation = false;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                //move our agent
+                agent.SetDestination(hit.point);
+            }
+        }
+        if (agent.remainingDistance > agent.stoppingDistance)
+        {
+            character.Move(agent.desiredVelocity, false, false);
+        }
+        else 
+        {
+            character.Move(Vector3.zero, false, false);
+        }
+    }
+}
